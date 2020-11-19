556eb5abf3f2b17dad90f26931d26e4966b805e6
Jan Krebs <jan.krebs@meissa-gmbh.de>
2020-11-19 16:01:11 +0100

    add backup and restore scripts

---
+++ infrastrucure/docker/image/resources/restore.sh
@@ -0,0 +1,12 @@
+# Restore Nextcloud Filesystem
+# Reads restore snapshot_ID from first CLI Argument
+restic -r $RESTIC_REPOSITORY restore $1 --target /
+
+# Delete DB 
+psql -d template1 -h $POSTGRES_SERVICE -p $POSTGRES_PORT -U $(cat ${POSTGRES_USER_FILE}) --no-password -c "DROP DATABASE \"cloud\";"
+# Create DB again
+psql -d template1 -h $POSTGRES_SERVICE -p $POSTGRES_PORT -U $(cat ${POSTGRES_USER_FILE}) --no-password -c "CREATE DATABASE \"cloud\";"
+# create folder from db backup
+restic -r $RESTIC_REPOSITORY restore $2 --target test-stdin-$2
+# read folder and restore db entries
+psql -d $(cat ${POSTGRES_DB_FILE}) -h $POSTGRES_SERVICE -p $POSTGRES_PORT -U $(cat ${POSTGRES_USER_FILE}) --no-password < test-stdin-$2
