bcd04af38bdebcc2f7639e4caa0a95d2d79a1949
Clemens Geibel <clemens.geibel@meissa-gmbh.de>
2020-11-19 15:26:20 +0100

    Use file secrets where necessary

--- infrastrucure/docker/image/resources/create_pg_pass.sh
+++ infrastrucure/docker/image/resources/create_pg_pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "${POSTGRES_HOST}:${POSTGRES_DB}:${POSTGRES_USER}:${DB_PASSWORD}" > /root/.pg_pass
-echo "${POSTGRES_HOST}:template1:${POSTGRES_USER}:${DB_PASSWORD}" >> /root/.pg_pass
+echo "${POSTGRES_HOST}:$(cat ${POSTGRES_DB_FILE}):$(cat ${POSTGRES_USER_FILE}):$(cat ${DB_PASSWORD_FILE})" > /root/.pg_pass
+echo "${POSTGRES_HOST}:template1:$(cat ${POSTGRES_USER_FILE}):$(cat ${DB_PASSWORD_FILE})" >> /root/.pg_pass
 chmod 0600 /root/.pg_pass
