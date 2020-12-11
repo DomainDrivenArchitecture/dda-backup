f4f629098e0df31326a8dd865ac0e77987ba744c
jem <michael.jerger@meissa-gmbh.de>
2020-12-11 17:55:18 +0100

    fix distinguishing backup-types by different tags

--- infrastrucure/docker/image/resources/backup.sh
+++ infrastrucure/docker/image/resources/backup.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 # backup database dump
 pg_dump -d $(cat ${POSTGRES_DB_FILE}) -h  $POSTGRES_SERVICE -p $POSTGRES_PORT -U $(cat ${POSTGRES_USER_FILE}) --no-password --serializable-deferrable --clean --no-privileges | \
-restic -r $RESTIC_REPOSITORY --verbose backup --stdin --tag DB_Backup
+restic -r $RESTIC_REPOSITORY/db backup --stdin
 
 # backup nextcloud filesystem
-restic -r $RESTIC_REPOSITORY backup /var/backups/ --tag Nextcloud_Filesystem
+restic -r $RESTIC_REPOSITORY/files backup /var/backups/
--- infrastrucure/docker/image/resources/init.sh
+++ infrastrucure/docker/image/resources/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-restic -r $RESTIC_REPOSITORY --verbose init
+restic -r $RESTIC_REPOSITORY/db --verbose init
+restic -r $RESTIC_REPOSITORY/files --verbose init
