a040a87f865a06f6ab773a967786edaeeb1b4734
jem <michael.jerger@meissa-gmbh.de>
2021-03-12 15:40:26 +0100

    cleanup the image

--- infrastructure/docker/image/resources/install.sh
+++ infrastructure/docker/image/resources/install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-apt-get update > /dev/null;
-apt-get install -qqy ca-certificates curl wget gnupg > /dev/null
-sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt focal-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+function main() {
+    apt-get update > /dev/null;
+    apt-get install -qqy ca-certificates curl wget gnupg > /dev/null
+    sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt focal-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
-apt-get update > /dev/null;
-apt-get -qqy install wget postgresql-client-13 restic > /dev/null;
+    apt-get update > /dev/null;
+    apt-get -qqy install wget postgresql-client-13 restic > /dev/null;
 
-update-ca-certificates
+    update-ca-certificates
 
-install -m 0400 /tmp/functions.sh /usr/local/lib/
-install -m 0400 /tmp/pg-functions.sh /usr/local/lib/
-install -m 0400 /tmp/file-functions.sh /usr/local/lib/
+    install -m 0400 /tmp/functions.sh /usr/local/lib/
+    install -m 0400 /tmp/pg-functions.sh /usr/local/lib/
+    install -m 0400 /tmp/file-functions.sh /usr/local/lib/
+
+    cleanupDocker
+}
+
+source /tmp/install_functions.sh
+main
